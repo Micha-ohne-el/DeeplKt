2ba283cea33b18803e4eaf2cd7a3a69ccc150e56
Micha Lehmann <michalehmann0112@gmail.com>
2023-05-08 14:36:49 +0200

    Restructure tests to allow running on Kotlin/JS
    This is simply a constraint of the Kotest engine.
    Kotlin/JS tests may only use flat specs (`FunSpec`/`ShouldSpec`/`StringSpec`).

--- src/commonTest/kotlin/DeeplClientTest.kt
+++ src/commonTest/kotlin/DeeplClientTest.kt
@@ -1,6 +1,6 @@
 package moe.micha.deeplkt
 
-import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldMatch
@@ -9,147 +9,137 @@ import io.ktor.client.engine.mock.respondOk
 import io.ktor.http.HttpMethod
 import io.ktor.http.URLProtocol
 
-class DeeplClientTest : DescribeSpec() {
+class DeeplClientTest : StringSpec() {
     init {
         val authKey = "01234567-89AB-CDEF-0123-456789ABCDEF"
         val freeAuthKey = "$authKey:fx"
 
+        lateinit var client: DeeplClient
         lateinit var engineSpy: MockEngine
+
+        suspend fun send(key: String = authKey): MockEngine {
+            DeeplClient(key, engineSpy).translate(text = "text", targetLang = TargetLang.AmericanEnglish)
+
+            return engineSpy
+        }
 
         beforeEach {
             engineSpy = MockEngine {
                 respondOk()
             }
+            client = DeeplClient(authKey, engineSpy)
         }
 
-        describe("translate") {
-            context("request") {
-                suspend fun send(key: String = authKey): MockEngine {
-                    DeeplClient(key, engineSpy).translate(text = "text", targetLang = TargetLang.AmericanEnglish)
-
-                    return engineSpy
-                }
-
-                it("uses HTTPS") {
-                    send().requestHistory.forAll {
-                        it.url.protocol shouldBe URLProtocol.HTTPS
-                    }
-                }
-
-                it("issues a POST request") {
-                    send().requestHistory.forAll {
-                        it.method shouldBe HttpMethod.Post
-                    }
-                }
-
-                it("calls v2 endpoint") {
-                    send().requestHistory.forAll {
-                        it.url.encodedPath shouldBe "/v2/translate"
-                    }
-                }
-
-                it("calls paid API for paid account") {
-                    send().requestHistory.forAll {
-                        it.url.host shouldBe "api.deepl.com"
-                    }
-                }
-
-                it("calls free API for free account") {
-                    send(freeAuthKey).requestHistory.forAll {
-                        it.url.host shouldBe "api-free.deepl.com"
-                    }
-                }
+        "translate > request > uses HTTPS" {
+            send().requestHistory.forAll {
+                it.url.protocol shouldBe URLProtocol.HTTPS
             }
+        }
 
-            context("parameters") {
-                lateinit var client: DeeplClient
+        "translate > request > issues a POST request" {
+            send().requestHistory.forAll {
+                it.method shouldBe HttpMethod.Post
+            }
+        }
 
-                beforeEach {
-                    client = DeeplClient(authKey, engineSpy)
-                }
+        "translate > request > calls v2 endpoint" {
+            send().requestHistory.forAll {
+                it.url.encodedPath shouldBe "/v2/translate"
+            }
+        }
 
-                it("accepts targetLang parameter") {
-                    client.translate("", targetLang = TargetLang.Dutch)
+        "translate > request > calls paid API for paid account" {
+            send().requestHistory.forAll {
+                it.url.host shouldBe "api.deepl.com"
+            }
+        }
 
-                    engineSpy.requestHistory.forAll {
-                        it.url.parameters["target_lang"] shouldBe "NL"
-                    }
-                }
+        "translate > request > calls free API for free account" {
+            send(freeAuthKey).requestHistory.forAll {
+                it.url.host shouldBe "api-free.deepl.com"
+            }
+        }
 
-                it("accepts sourceLang parameter") {
-                    client.translate("", TargetLang.Dutch, sourceLang = SourceLang.French)
+        "translate > parameters > accepts targetLang parameter" {
+            client.translate("", targetLang = TargetLang.Dutch)
 
-                    engineSpy.requestHistory.forAll {
-                        it.url.parameters["source_lang"] shouldBe "FR"
-                    }
-                }
+            engineSpy.requestHistory.forAll {
+                it.url.parameters["target_lang"] shouldBe "NL"
+            }
+        }
 
-                it("accepts splitSentences parameter") {
-                    client.translate("", TargetLang.Dutch, splitSentences = SplitSentences.Never)
+        "translate > parameters > accepts sourceLang parameter" {
+            client.translate("", TargetLang.Dutch, sourceLang = SourceLang.French)
 
-                    engineSpy.requestHistory.forAll {
-                        it.url.parameters["split_sentences"] shouldBe "0"
-                    }
-                }
+            engineSpy.requestHistory.forAll {
+                it.url.parameters["source_lang"] shouldBe "FR"
+            }
+        }
 
-                it("accepts preserveFormatting parameter") {
-                    client.translate("", TargetLang.Dutch, preserveFormatting = PreserveFormatting.Yes)
+        "translate > parameters > accepts splitSentences parameter" {
+            client.translate("", TargetLang.Dutch, splitSentences = SplitSentences.Never)
 
-                    engineSpy.requestHistory.forAll {
-                        it.url.parameters["preserve_formatting"] shouldBe "1"
-                    }
-                }
+            engineSpy.requestHistory.forAll {
+                it.url.parameters["split_sentences"] shouldBe "0"
+            }
+        }
 
-                it("accepts formality parameter") {
-                    client.translate("", TargetLang.Dutch, formality = Formality.More)
+        "translate > parameters > accepts preserveFormatting parameter" {
+            client.translate("", TargetLang.Dutch, preserveFormatting = PreserveFormatting.Yes)
 
-                    engineSpy.requestHistory.forAll {
-                        it.url.parameters["formality"] shouldBe "prefer_more"
-                    }
-                }
+            engineSpy.requestHistory.forAll {
+                it.url.parameters["preserve_formatting"] shouldBe "1"
+            }
+        }
 
-                it("accepts tagHandling parameter") {
-                    client.translate("", TargetLang.Dutch, tagHandling = TagHandling.Xml)
+        "translate > parameters > accepts formality parameter" {
+            client.translate("", TargetLang.Dutch, formality = Formality.More)
 
-                    engineSpy.requestHistory.forAll {
-                        it.url.parameters["tag_handling"] shouldBe "xml"
-                    }
-                }
+            engineSpy.requestHistory.forAll {
+                it.url.parameters["formality"] shouldBe "prefer_more"
+            }
+        }
 
-                it("accepts nonSplittingTags parameter") {
-                    client.translate("", TargetLang.Dutch, nonSplittingTags = setOf("one", "two"))
-                    client.translate("", TargetLang.Dutch, nonSplittingTags = listOf("one", "two"))
+        "translate > parameters > accepts tagHandling parameter" {
+            client.translate("", TargetLang.Dutch, tagHandling = TagHandling.Xml)
 
-                    engineSpy.requestHistory.forAll {
-                        it.url.parameters["non_splitting_tags"] shouldMatch """one,two|two,one"""
-                    }
-                }
+            engineSpy.requestHistory.forAll {
+                it.url.parameters["tag_handling"] shouldBe "xml"
+            }
+        }
 
-                it("accepts outlineDetection parameter") {
-                    client.translate("", TargetLang.Dutch, outlineDetection = OutlineDetection.Disabled)
+        "translate > parameters > accepts nonSplittingTags parameter" {
+            client.translate("", TargetLang.Dutch, nonSplittingTags = setOf("one", "two"))
+            client.translate("", TargetLang.Dutch, nonSplittingTags = listOf("one", "two"))
 
-                    engineSpy.requestHistory.forAll {
-                        it.url.parameters["outline_detection"] shouldBe "0"
-                    }
-                }
+            engineSpy.requestHistory.forAll {
+                it.url.parameters["non_splitting_tags"] shouldMatch """one,two|two,one"""
+            }
+        }
 
-                it("accepts splittingTags parameter") {
-                    client.translate("", TargetLang.Dutch, splittingTags = setOf("test1", "test2"))
-                    client.translate("", TargetLang.Dutch, splittingTags = listOf("test1", "test2"))
+        "translate > parameters > accepts outlineDetection parameter" {
+            client.translate("", TargetLang.Dutch, outlineDetection = OutlineDetection.Disabled)
 
-                    engineSpy.requestHistory.forAll {
-                        it.url.parameters["splitting_tags"] shouldMatch """test1,test2|test2,test1"""
-                    }
-                }
+            engineSpy.requestHistory.forAll {
+                it.url.parameters["outline_detection"] shouldBe "0"
+            }
+        }
 
-                it("accepts ignoreTags parameter") {
-                    client.translate("", TargetLang.Dutch, ignoreTags = setOf("abc", "def"))
-                    client.translate("", TargetLang.Dutch, ignoreTags = listOf("abc", "def"))
+        "translate > parameters > accepts splittingTags parameter" {
+            client.translate("", TargetLang.Dutch, splittingTags = setOf("test1", "test2"))
+            client.translate("", TargetLang.Dutch, splittingTags = listOf("test1", "test2"))
 
-                    engineSpy.requestHistory.forAll {
-                        it.url.parameters["ignore_tags"] shouldMatch """abc,def|def,abc"""
-                    }
-                }
+            engineSpy.requestHistory.forAll {
+                it.url.parameters["splitting_tags"] shouldMatch """test1,test2|test2,test1"""
+            }
+        }
+
+        "translate > parameters > accepts ignoreTags parameter" {
+            client.translate("", TargetLang.Dutch, ignoreTags = setOf("abc", "def"))
+            client.translate("", TargetLang.Dutch, ignoreTags = listOf("abc", "def"))
+
+            engineSpy.requestHistory.forAll {
+                it.url.parameters["ignore_tags"] shouldMatch """abc,def|def,abc"""
             }
         }
     }
