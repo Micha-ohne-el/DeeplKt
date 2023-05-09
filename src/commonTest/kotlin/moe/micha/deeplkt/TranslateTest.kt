7026b70641cdc128a231b8d481ec6df34d1ffd86
Micha Lehmann <michalehmann0112@gmail.com>
2023-05-09 16:14:14 +0200

    Refactor tests

--- src/commonTest/kotlin/moe/micha/deeplkt/TranslateTest.kt
+++ src/commonTest/kotlin/moe/micha/deeplkt/TranslateTest.kt
@@ -21,7 +21,7 @@ import moe.micha.deeplkt.translate.TagHandling
 import moe.micha.deeplkt.translate.TranslateResponse
 import moe.micha.deeplkt.translate.Translation
 
-class DeeplClientTest : StringSpec() {
+class TranslateTest : StringSpec() {
     init {
         beforeEach {
             engineSpy = MockEngine {
@@ -36,43 +36,43 @@ class DeeplClientTest : StringSpec() {
             client = DeeplClient(authKey, engineSpy)
         }
 
-        "translate > request > uses HTTPS" {
+        "uses HTTPS" {
             send().requestHistory.forAll {
                 it.url.protocol shouldBe URLProtocol.HTTPS
             }
         }
 
-        "translate > request > issues a POST request" {
+        "issues a POST request" {
             send().requestHistory.forAll {
                 it.method shouldBe HttpMethod.Post
             }
         }
 
-        "translate > request > calls v2 endpoint" {
+        "calls v2 endpoint" {
             send().requestHistory.forAll {
                 it.url.encodedPath shouldBe "/v2/translate"
             }
         }
 
-        "translate > request > calls paid API for paid account" {
+        "calls paid API for paid account" {
             send().requestHistory.forAll {
                 it.url.host shouldBe "api.deepl.com"
             }
         }
 
-        "translate > request > calls free API for free account" {
+        "calls free API for free account" {
             send(freeAuthKey).requestHistory.forAll {
                 it.url.host shouldBe "api-free.deepl.com"
             }
         }
 
-        "translate > request > contains auth key" {
+        "request contains auth key" {
             send().requestHistory.forAll {
                 it.headers["Authorization"] shouldBe "DeepL-Auth-Key $authKey"
             }
         }
 
-        "translate > parameters > accepts targetLang parameter" {
+        "parameters include targetLang" {
             client.translate("", targetLang = TargetLang.Dutch)
 
             engineSpy.requestHistory.forAll {
@@ -80,7 +80,7 @@ class DeeplClientTest : StringSpec() {
             }
         }
 
-        "translate > parameters > accepts sourceLang parameter" {
+        "parameters include sourceLang" {
             client.translate("", TargetLang.Dutch, sourceLang = SourceLang.French)
 
             engineSpy.requestHistory.forAll {
@@ -88,7 +88,7 @@ class DeeplClientTest : StringSpec() {
             }
         }
 
-        "translate > parameters > accepts splitSentences parameter" {
+        "parameters include splitSentences" {
             client.translate("", TargetLang.Dutch, splitSentences = SplitSentences.Never)
 
             engineSpy.requestHistory.forAll {
@@ -96,7 +96,7 @@ class DeeplClientTest : StringSpec() {
             }
         }
 
-        "translate > parameters > accepts preserveFormatting parameter" {
+        "parameters include preserveFormatting" {
             client.translate("", TargetLang.Dutch, preserveFormatting = PreserveFormatting.Yes)
 
             engineSpy.requestHistory.forAll {
@@ -104,7 +104,7 @@ class DeeplClientTest : StringSpec() {
             }
         }
 
-        "translate > parameters > accepts formality parameter" {
+        "parameters include formality" {
             client.translate("", TargetLang.Dutch, formality = Formality.More)
 
             engineSpy.requestHistory.forAll {
@@ -112,7 +112,7 @@ class DeeplClientTest : StringSpec() {
             }
         }
 
-        "translate > parameters > accepts tagHandling parameter" {
+        "parameters include tagHandling" {
             client.translate("", TargetLang.Dutch, tagHandling = TagHandling.Xml)
 
             engineSpy.requestHistory.forAll {
@@ -120,7 +120,7 @@ class DeeplClientTest : StringSpec() {
             }
         }
 
-        "translate > parameters > accepts nonSplittingTags parameter" {
+        "parameters include nonSplittingTags" {
             client.translate("", TargetLang.Dutch, nonSplittingTags = setOf("one", "two"))
             client.translate("", TargetLang.Dutch, nonSplittingTags = listOf("one", "two"))
 
@@ -129,7 +129,7 @@ class DeeplClientTest : StringSpec() {
             }
         }
 
-        "translate > parameters > accepts outlineDetection parameter" {
+        "parameters include outlineDetection" {
             client.translate("", TargetLang.Dutch, outlineDetection = OutlineDetection.Disabled)
 
             engineSpy.requestHistory.forAll {
@@ -137,7 +137,7 @@ class DeeplClientTest : StringSpec() {
             }
         }
 
-        "translate > parameters > accepts splittingTags parameter" {
+        "parameters include splittingTags" {
             client.translate("", TargetLang.Dutch, splittingTags = setOf("test1", "test2"))
             client.translate("", TargetLang.Dutch, splittingTags = listOf("test1", "test2"))
 
@@ -146,7 +146,7 @@ class DeeplClientTest : StringSpec() {
             }
         }
 
-        "translate > parameters > accepts ignoreTags parameter" {
+        "parameters include ignoreTags" {
             client.translate("", TargetLang.Dutch, ignoreTags = setOf("abc", "def"))
             client.translate("", TargetLang.Dutch, ignoreTags = listOf("abc", "def"))
 
@@ -155,35 +155,27 @@ class DeeplClientTest : StringSpec() {
             }
         }
 
-        "translate > parameters > accepts single text" {
-            client.translate("text", TargetLang.Dutch)
+        "can be called with single text" {
+            val result = client.translate("text", TargetLang.Dutch)
 
             engineSpy.requestHistory.forAll {
                 it.formBody.formData.getAll("text") shouldBe listOf("text")
             }
-        }
-
-        "translate > parameters > accepts multiple texts" {
-            client.translate("text1", "text2", "text3", targetLang = TargetLang.Dutch)
-
-            engineSpy.requestHistory.forAll {
-                it.formBody.formData.getAll("text") shouldBe listOf("text1", "text2", "text3")
-            }
-        }
-
-        "translate > result > is a Translation for single text" {
-            val result = client.translate("my-text", TargetLang.Dutch)
 
             result.shouldBeInstanceOf<Translation>()
         }
 
-        "translate > result > is a TranslateResponse for single text" {
-            val result = client.translate("my-text", "my-other-text", targetLang = TargetLang.Dutch)
+        "can be called with multiple texts" {
+            val result = client.translate("text1", "text2", "text3", targetLang = TargetLang.Dutch)
+
+            engineSpy.requestHistory.forAll {
+                it.formBody.formData.getAll("text") shouldBe listOf("text1", "text2", "text3")
+            }
 
             result.shouldBeInstanceOf<TranslateResponse>()
         }
 
-        "translate > result > texts are ordered correctly" {
+        "result texts are ordered correctly" {
             engineSpy = MockEngine {
                 respond(
                     """
