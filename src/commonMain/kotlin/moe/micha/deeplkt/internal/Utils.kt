3f9215a7630f03876e3fb988ba35234d729235d3
Micha Lehmann <michalehmann0112@gmail.com>
2023-11-04 18:10:49 +0100

    Make internal things actually internal

--- src/commonMain/kotlin/moe/micha/deeplkt/internal/Utils.kt
+++ src/commonMain/kotlin/moe/micha/deeplkt/internal/Utils.kt
@@ -3,13 +3,13 @@ package moe.micha.deeplkt.internal
 import io.ktor.client.request.forms.FormBuilder
 import io.ktor.util.StringValuesBuilder
 
-fun StringValuesBuilder.append(name: String, value: String?) {
+internal fun StringValuesBuilder.append(name: String, value: String?) {
     if (value != null) {
         append(name, value)
     }
 }
 
-fun FormBuilder.append(key: String, value: String?) {
+internal fun FormBuilder.append(key: String, value: String?) {
     if (value != null) {
         append(key, value)
     }
