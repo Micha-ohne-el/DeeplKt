4e34a33ffbd0371aedd4867af86bb3a71dc04aeb
Micha Lehmann <michalehmann0112@gmail.com>
2023-12-01 18:53:08 +0100

    fix!: Make `HttpStatusCode.QuotaExceeded` internal as it should be

--- src/commonMain/kotlin/moe/micha/deeplkt/internal/QuotaExceeded.kt
+++ src/commonMain/kotlin/moe/micha/deeplkt/internal/QuotaExceeded.kt
@@ -2,4 +2,4 @@ package moe.micha.deeplkt.internal
 
 import io.ktor.http.HttpStatusCode
 
-val HttpStatusCode.Companion.QuotaExceeded get() = HttpStatusCode(456, "Quota Exceeded")
+internal val HttpStatusCode.Companion.QuotaExceeded get() = HttpStatusCode(456, "Quota Exceeded")
